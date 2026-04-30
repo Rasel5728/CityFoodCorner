7a335ad0185d31c9067192a451486b77e673631a
opusutradhar52 <opusutradhar2004@gmail.com>
2026-05-01 03:45:16 +0600

    Menu design and controller update

--- src/main/java/com/first/mainfrontpage/MenuController.java
+++ src/main/java/com/first/mainfrontpage/MenuController.java
@@ -6,41 +6,36 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.VBox;
 
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class MenuController implements Initializable {
-    public VBox leftScrool;
-    public VBox rightScrool;
+
     @FXML
     private ScrollPane scrollbar;
     @FXML
-    private GridPane menulist;
+    private GridPane menuGrid;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-       removeScrollBar(scrollbar);
+        removeScrollBar(scrollbar);
         try {
             addlist(13);
-
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
-    void addlist(int item) throws IOException {
-        for(int i=0; i<item; i++){
-            if(i%2 != 0){
-                rightScrool.getChildren().add(loadList());
-            }
-            else {
-                leftScrool.getChildren().add(loadList());
-            }
-        }
 
+    void addlist(int item) throws IOException {
+        for (int i = 0; i < item; i++) {
+            int col = i % 2;
+            int row = i / 2;
+            menuGrid.add(loadList(), col, row);
+        }
     }
+
     AnchorPane loadList() throws IOException {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("foodCard.fxml"));
         AnchorPane card = loader.load();
@@ -52,5 +47,4 @@ public class MenuController implements Initializable {
         scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
         scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
     }
-
 }
